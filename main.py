5d0428fe111afb5101a6bb0b3936a4c4e8f9f7ee
Vesal <vwfayy@gmail.com>
2025-08-02 13:31:35 +0330

    main.py is now functional, webcam_Cap...py is now a modular file with webcam() function. you can run program with main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Sinam project is a FA lang OCR to read and save books in real-time with dict and fail managment
+By: Zevzda
+"""
+import cv2
+from ocr.webcam_Capture import webcam
+def main():
+    webcam()
+main()
